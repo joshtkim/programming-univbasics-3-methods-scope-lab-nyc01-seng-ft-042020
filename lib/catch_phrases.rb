ad1a4eec44601970ec1da1baebe131aead493bf9
Joshua Kim <jykim92@gmail.com>
2020-04-07 22:05:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,7 @@
-$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$status = "Thank You Mario! But Our Princess Is In Another Castle!"
 
 def mario
   phrase = "It's-a me, Mario!"
-
   puts phrase
 end
 
