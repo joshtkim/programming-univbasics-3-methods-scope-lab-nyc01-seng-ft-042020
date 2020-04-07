55908f546deb78a9fd2cba4ab3483156ae741a68
Joshua Kim <jykim92@gmail.com>
2020-04-07 22:05:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,13 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
   phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
   puts phrase
 end
 
 def toadstool
-  status = "It's Dangerous To Go Alone! Take This."
   puts status
 end
+
+def all_phrases
